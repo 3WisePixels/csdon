81b0dbd688f78b3eaf494ce653a54ebd6e168906
Sean Obienu <sean@3wp.io>
2017-10-11 13:47:28 +0100

    MGun

---
+++ reg/server/mgun.js
@@ -0,0 +1,41 @@
+Meteor.startup(function(){
+    Meteor.Mailgun.config({
+      username: 'postmaster@3wp.io',
+      password: 'Rule#M4Car81n3'
+    });
+    smtp = {
+        username: 'postmaster@3wp.io',
+        password: 'Rule#M4Car81n3',
+        server: 'smtp.mailgun.org',
+        port: 465
+    }
+
+    process.env.MAIL_URL = 'smtps://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+
+    //process.env.MAIL_URL = 'smtp://postmaster@3wp.io:Rule#M4Car81n3@smtp.mailgun.org:465';
+
+});
+
+  // In your server code: define a method that the client can call
+  Meteor.methods({
+    sendEmail: function (mailFields) {
+        console.log("about to send email...");
+        check([mailFields.to, mailFields.from, mailFields.subject, mailFields.text, mailFields.html], [String]);
+
+        // Let other method calls from the same client start running,
+        // without waiting for the email sending to complete.
+        this.unblock();
+
+        Meteor.Mailgun.send({
+            to: mailFields.to,
+            from: mailFields.from,
+            subject: mailFields.subject,
+            text: mailFields.text,
+            html: mailFields.html
+        });
+        console.log("email sent!");
+    }
+  });
+
+
+// Meteor.call('sendEmail',{to:'sean@3wp.io',from:'donotreply@csdon.org',subject:'Registration',text:'Success. You have been registered',html:''})
